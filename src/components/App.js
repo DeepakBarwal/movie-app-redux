d3002c86ed03014e64231215d6342af58446c544
DeepakBarwal <deepakbarwal98@gmail.com>
2021-01-22 21:10:16 +0530

    changing the state to add favourites as well

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,25 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
   componentDidMount() {
     const { store } = this.props;
     store.subscribe(() => {
+      // called after dispatch
       console.log('UPDATED');
       this.forceUpdate(); // anti
     });
     // make api call
     // dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
     console.log('state', this.props.store.getState());
   }
 
   render() {
-    console.log('RENDER');
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState(); // {list:[], fav:[]}
+    console.log('RENDER', this.props.store.getState());
     return (
       <div className='App'>
         <Navbar />
@@ -32,7 +31,7 @@ class App extends React.Component {
           </div>
 
           <div className='list'>
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
