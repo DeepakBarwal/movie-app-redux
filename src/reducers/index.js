e86e170021a6440caff3e0b25ec26a83155a892b
DeepakBarwal <deepakbarwal98@gmail.com>
2021-01-23 21:18:42 +0530

    combineReducers method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 import {
   ADD_MOVIES,
   ADD_TO_FAVOURITES,
@@ -11,7 +13,7 @@ const initialMoviesState = {
   showFavourites: false,
 };
 
-export default function movies(state = initialMoviesState, action) {
+export function movies(state = initialMoviesState, action) {
   // if (action.type === ADD_MOVIES) {
   //   return {
   //     ...state,
@@ -52,3 +54,26 @@ export default function movies(state = initialMoviesState, action) {
       return state;
   }
 }
+
+const initialSearchState = {
+  result: {},
+};
+export function search(state = initialSearchState, action) {
+  return state;
+}
+
+// const initialRootState = {
+//   movies: initialMoviesState,
+//   search: initialSearchState,
+// };
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+// Shorthand
+export default combineReducers({
+  movies,
+  search,
+});
